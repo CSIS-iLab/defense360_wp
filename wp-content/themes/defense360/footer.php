31d594acabffb2230fd5bdef8cfb9d3ad4a9ff9b
Jacque Schrag <jnschrag@gmail.com>
2017-05-12 11:19:18 -0400

    Initial d360 theme commit

---
+++ wp-content/themes/defense360/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package defense360
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'defense360' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'defense360' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'defense360' ), 'defense360', '<a href="https://automattic.com/" rel="designer">CSIS iDeas Lab</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
