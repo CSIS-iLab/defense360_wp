31ba703dc624fc5e1cbb76268ea515a52a80ecab
Jacque Schrag <jnschrag@gmail.com>
2017-05-15 15:23:25 -0400

    Create style.css with Travis

--- test/travis.rb
+++ test/travis.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-result = `sass wp-content/themes/defense360/sass/style.scss built.css`
+result = `sass wp-content/themes/defense360/sass/style.scss wp-content/themes/defense360/style.css`
 raise result unless $?.to_i == 0
 raise "When compiled the module should output some CSS" unless File.exists?('built.css')
 puts "Regular compile worked successfully"
