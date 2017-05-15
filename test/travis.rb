8a779458986731c460bc01c40cee770f0d63d060
Jacque Schrag <jnschrag@gmail.com>
2017-05-15 15:24:53 -0400

    Update Travis test

--- test/travis.rb
+++ test/travis.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 result = `sass wp-content/themes/defense360/sass/style.scss wp-content/themes/defense360/style.css`
 raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('built.css')
+raise "When compiled the module should output some CSS" unless File.exists?('style.css')
 puts "Regular compile worked successfully"
