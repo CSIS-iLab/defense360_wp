9e9060e58905ad94ba72cef84fa7fe2c7942f0b4
Jacque Schrag <jnschrag@gmail.com>
2017-05-22 15:43:17 -0400

    Add search to header

--- wp-content/themes/defense360/header.php
+++ wp-content/themes/defense360/header.php
@@ -37,6 +37,7 @@
 			</div>
 			<div class="header-searchContainer col-xs-2 visible-xs">
 				<i class="icon icon-search"></i>
+				<span class="close-icon"></span>
 			</div>
 		</div><!-- .content-wrapper -->
 		<div class="navigation-container">
@@ -73,6 +74,26 @@
 				} ?>
 			</div><!-- .nav-social -->
 		</div><!-- .navigation-container -->
+		<div class="header-searchFormContainer">
+			<div class="header-searchForm content-wrapper row flex-center__y">
+				<div class="header-searchInputContainer col-xs-12 col-md-11">
+					<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+					    <label>
+					        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+					        <input type="search" class="search-field"
+					            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+					            value="<?php echo get_search_query() ?>" name="s"
+					            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+					    </label>
+					    <input type="submit" class="search-submit"
+					        value="<?php echo esc_attr_x( '&#8594;', 'submit button' ) ?>" />
+					</form>
+				</div>
+				<div class="search-closeContainer hidden-xs col-md-1">
+					<span class="close-icon"></span>
+				</div>
+			</div>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content">
