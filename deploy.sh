67db4bc11eb4e3c6bedac00c9247edc5378fe515
Jacque Schrag <jnschrag@gmail.com>
2017-06-12 16:20:45 -0400

    Update git repo name

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ chmod 600 /tmp/d360_travis
 eval "$(ssh-agent -s)" # Start the ssh agent
 ssh-add /tmp/d360_travis
 git remote add defense360_wp git@git.wpengine.com:staging/defense360.git # add remote for staging site
-git push wpengine-staging master #deploy to staging site
+git push defense360_wp master #deploy to staging site
