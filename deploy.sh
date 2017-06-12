559173ca717b55f640a23c6465540a971b1d9bab
jnschrag <jnschrag@gmail.com>
2017-06-12 19:27:10 -0400

    checkout and commit changes to local master

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@ chmod 600 /tmp/d360_travis
 eval "$(ssh-agent -s)" # Start the ssh agent
 ssh-add /tmp/d360_travis
 git remote add defense360_wp git@git.wpengine.com:staging/defense360.git # add remote for staging site
+git checkout master
+git add --all
+git commit -m "compiled assets"
 git push defense360_wp master #deploy to staging site
