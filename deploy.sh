ff53ea3d1fae3f4033c63c2fcec4b3837307eb49
jnschrag <jnschrag@gmail.com>
2017-06-12 19:45:12 -0400

    Add comments & background test

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ ssh-add /tmp/d360_travis
 git remote add defense360_wp git@git.wpengine.com:staging/defense360.git # add remote for staging site
 git add .
 git commit -m "compiled assets"
-git fetch --unshallow
-git push defense360_wp development:master #deploy to staging site
+git fetch --unshallow # fetch all repo history or wpengine complains
+git push defense360_wp development:master #deploy to staging site from development to master
