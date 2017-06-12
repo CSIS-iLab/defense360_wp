d8f64e54a17861704b523d6332d2cc801e8c5ce6
jnschrag <jnschrag@gmail.com>
2017-06-12 19:31:11 -0400

    show ref

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ eval "$(ssh-agent -s)" # Start the ssh agent
 ssh-add /tmp/d360_travis
 git remote add defense360_wp git@git.wpengine.com:staging/defense360.git # add remote for staging site
 git checkout master
-git add --all
+git show-ref
+git add .
 git commit -m "compiled assets"
 git push defense360_wp master #deploy to staging site
