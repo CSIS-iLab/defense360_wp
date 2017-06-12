742aa746557bc51b3e2b7925ca93a53c8f80dfed
jnschrag <jnschrag@gmail.com>
2017-06-12 19:51:29 -0400

    better logging

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@ chmod 600 /tmp/d360_travis
 eval "$(ssh-agent -s)" # Start the ssh agent
 ssh-add /tmp/d360_travis
 git remote add defense360_wp git@git.wpengine.com:staging/defense360.git # add remote for staging site
+git fetch --unshallow # fetch all repo history or wpengine complain
+git status
+git checkout development
 git add .
 git commit -m "compiled assets"
-git fetch --unshallow # fetch all repo history or wpengine complains
 git push defense360_wp development:master #deploy to staging site from development to master
