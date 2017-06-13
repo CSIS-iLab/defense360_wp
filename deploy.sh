1a10c38a5f44a65d9ddcd4498ec8ce6bc0b93307
jnschrag <jnschrag@gmail.com>
2017-06-12 20:06:24 -0400

    Add comments to deploy script & revert variables.scss

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ eval "$(ssh-agent -s)" # Start the ssh agent
 ssh-add /tmp/d360_travis
 git remote add defense360_wp git@git.wpengine.com:staging/defense360.git # add remote for staging site
 git fetch --unshallow # fetch all repo history or wpengine complain
-git status
-git checkout development
-git add wp-content/themes/defense360/*.css -f
-git commit -m "compiled assets"
+git status # check git status
+git checkout development # switch to development branch
+git add wp-content/themes/defense360/*.css -f # force all compiled CSS files to be added
+git commit -m "compiled assets" # commit the compiled CSS files
 git push -f defense360_wp development:master #deploy to staging site from development to master
