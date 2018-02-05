98bdd46d7d48caca5a1c0de0d889fff62d0d89fd
Jacque Schrag <jnschrag@gmail.com>
2018-02-05 11:37:25 -0500

    Update D360 TravisCI deployment. Deploy from the master branch instead of development.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ssh-add /tmp/d360_travis
 git remote add defense360_wp git@git.wpengine.com:staging/defense360.git # add remote for staging site
 git fetch --unshallow # fetch all repo history or wpengine complain
 git status # check git status
-git checkout development # switch to development branch
+git checkout master # switch to master branch
 git add wp-content/themes/defense360/*.css -f # force all compiled CSS files to be added
 git commit -m "compiled assets" # commit the compiled CSS files
-git push -f defense360_wp development:master #deploy to staging site from development to master
+git push -f defense360_wp master:master #deploy to staging site from master to master
