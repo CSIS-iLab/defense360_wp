283a88077a680a737f87fee640a384afad5c652d
jdromero88 <jdromerov88@gmail.com>
2021-06-21 19:32:25 -0400

    Working on optional featured image

---
+++ wp-content/themes/defense360/templates/template-no-image.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: No Feature Image
+ * Template Post Type: post, page
+ *
+ * @package CSIS iLab
+ * @subpackage @package defense360
+ * @since 1.0
+ */
+
+get_template_part( 'single' );
