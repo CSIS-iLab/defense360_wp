de72f88c92fc2b197418ad651ee0e637e8a82966
Lindsay Allison <lindsayallison@macbook-pro.mynetworksettings.com>
2025-04-16 14:17:20 -0400

    Adjust date of header scroll js file in enqueue script function

--- wp-content/themes/defense360/functions.php
+++ wp-content/themes/defense360/functions.php
@@ -103,7 +103,7 @@ function defense360_scripts() {
 
 	wp_enqueue_script( 'defense360-mobile-menu', get_template_directory_uri() . '/assets/js/mobile-menu.min.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'defense360-header-scroll', get_template_directory_uri() . '/assets/js/header-scroll.min.js', array(), '20250402', true );
+	wp_enqueue_script( 'defense360-header-scroll', get_template_directory_uri() . '/assets/js/header-scroll.min.js', array(), '20250416', true );
 
 	wp_enqueue_script( 'defense360-hide-header-mobile', get_template_directory_uri() . '/assets/js/hide-header-mobile.min.js', array(), '20151215', true );
 
